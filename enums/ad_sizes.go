be9a4514c0c73a41e03fe8d2af1e0f313fb285e2
saman-alayee <saman.alaii10@gmail.com>
2024-12-10 17:15:20 +0330

    fix bug of sizes

--- enums/ad_sizes.go
+++ enums/ad_sizes.go
@@ -10,20 +10,20 @@ type AdSize struct {
 
 // Define a map for converting integer ad sizes to their respective width and height.
 var adSizesMap = map[int]AdSize{
-    1:  {192, 192},    // SIZE_ICON
-    2:  {720, 480},    // SIZE1
-    3:  {728, 90},     // SIZE2
-    4:  {480, 320},    // SIZE3
-    5:  {492, 328},    // SIZE4
-    6:  {468, 60},     // SIZE5
-    7:  {360, 240},    // SIZE6
-    8:  {320, 100},    // SIZE7
-    9:  {320, 50},     // SIZE8
-    10: {300, 250},    // SIZE9
-    11: {295, 98},     // SIZE10
-    12: {160, 600},    // SIZE11
-    13: {720, 360},    // SIZE12
-    14: {1200, 1200},  // SIZE_INTERSTITIAL
+    0:  {192, 192},    // SIZE_ICON
+    1:  {720, 480},    // SIZE1
+    2:  {728, 90},     // SIZE2
+    3:  {480, 320},    // SIZE3
+    4:  {492, 328},    // SIZE4
+    5:  {468, 60},     // SIZE5
+    6:  {360, 240},    // SIZE6
+    7:  {320, 100},    // SIZE7
+    8:  {320, 50},     // SIZE8
+    9:  {300, 250},    // SIZE9
+    10: {295, 98},     // SIZE10
+    11: {160, 600},    // SIZE11
+    12: {720, 360},    // SIZE12
+    13: {1200, 1200},  // SIZE_INTERSTITIAL
 }
 
 // GetAdSizeByID returns the corresponding AdSize based on the provided ID (adsize).
